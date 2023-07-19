fc0c071436e4c0dbf5097bdba3e2d6b2afcfa808
Rio Pramana <riopramana1021@gmail.com>
2023-07-19 15:00:00 +0700

    Add customer search to order list

--- frontend/src/OrderList.tsx
+++ frontend/src/OrderList.tsx
@@ -3,6 +3,7 @@ import { listOrders, type Order } from "./api"
 
 export function OrderList({ onSelect }: { onSelect: (id: number) => void }) {
   const [orders, setOrders] = useState<Order[]>([])
+  const [search, setSearch] = useState("")
   const [error, setError] = useState<string | null>(null)
 
   useEffect(() => {
@@ -13,24 +14,35 @@ export function OrderList({ onSelect }: { onSelect: (id: number) => void }) {
 
   if (error) return <p>Couldn't load orders: {error}</p>
 
+  const visible = orders.filter((o) =>
+    o.customer_name.toLowerCase().includes(search.toLowerCase())
+  )
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Order</th>
-          <th>Customer</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        {orders.map((o) => (
-          <tr key={o.id} onClick={() => onSelect(o.id)} style={{ cursor: "pointer" }}>
-            <td>#{o.id}</td>
-            <td>{o.customer_name}</td>
-            <td>{o.status}</td>
+    <div>
+      <input
+        placeholder="Search by customer"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>Order</th>
+            <th>Customer</th>
+            <th>Status</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {visible.map((o) => (
+            <tr key={o.id} onClick={() => onSelect(o.id)} style={{ cursor: "pointer" }}>
+              <td>#{o.id}</td>
+              <td>{o.customer_name}</td>
+              <td>{o.status}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
